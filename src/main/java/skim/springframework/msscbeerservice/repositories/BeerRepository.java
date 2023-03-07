08e752c99858d81173ec1147329a1dd07c89988b
Sunpil Kim <ezweb28@gmail.com>
2023-03-06 23:01:46 -0500

    Added Brewing Service to Trigger Brewing, closes #25

--- src/main/java/skim/springframework/msscbeerservice/repositories/BeerRepository.java
+++ src/main/java/skim/springframework/msscbeerservice/repositories/BeerRepository.java
@@ -2,13 +2,13 @@ package skim.springframework.msscbeerservice.repositories;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import skim.springframework.msscbeerservice.domain.Beer;
 import skim.springframework.msscbeerservice.web.model.BeerStyleEnum;
 
 import java.util.UUID;
 
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
     Page<Beer> findAllByBeerNameAndBeerStyle(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest);
 
     Page<Beer> findAllByBeerName(String beerName, PageRequest pageRequest);
