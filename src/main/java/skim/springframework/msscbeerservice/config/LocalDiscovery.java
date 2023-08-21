cf1749131243256b8709f1c81f85825d7649ef92
Sunpil Kim <ezweb28@gmail.com>
2023-08-20 23:20:07 -0500

    Configure Eureka Client, closes #33

---
+++ src/main/java/skim/springframework/msscbeerservice/config/LocalDiscovery.java
@@ -0,0 +1,12 @@
+package skim.springframework.msscbeerservice.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("local-discovery")
+@EnableDiscoveryClient
+@Configuration
+public class LocalDiscovery {
+
+}
