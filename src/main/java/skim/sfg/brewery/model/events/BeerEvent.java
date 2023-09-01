41e3d7602c97517f45a135b44409626b68eedeed
Sunpil Kim <ezweb28@gmail.com>
2023-08-31 21:46:26 -0500

    Update Inventory Service to use OpenFeign, closes #34

--- src/main/java/skim/sfg/brewery/model/events/BeerEvent.java
+++ src/main/java/skim/sfg/brewery/model/events/BeerEvent.java
@@ -1,6 +1,9 @@
 package skim.sfg.brewery.model.events;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import skim.sfg.brewery.model.BeerDto;
 
 import java.io.Serializable;
